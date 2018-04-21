a9724b6235fb8940db9908c1ca6155282a822535
jeronimonoriega <38385523+jeronimonoriega@users.noreply.github.com>
2018-04-21 15:33:39 -0500

    Update SELECT first_name, last_name, salary FROM Employee.sql

--- SELECT first_name, last_name, salary FROM Employee.sql
+++ SELECT first_name, last_name, salary FROM Employee.sql
@@ -22,7 +22,7 @@ SELECT last_name, job_id, salary FROM Employees
 WHERE job_id IN ('IT_PROG', 'SH_CLERK') AND salary NOT IN (4500,10000,15000);
 
 
-SELECT last_name FROM Employees WHERE last_name LIKE '______';
+SELECT last_name FROM Employees WHERE first_name LIKE '______';
 
 
 SELECT last_name FROM Employees WHERE last_name LIKE '__e%';
